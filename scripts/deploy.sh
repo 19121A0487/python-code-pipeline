b31047169f61fb704dbae1618ed82b294e5be9e6
Kadiri Sree Pathi Rajendra Reddy <86181726+19121A0487@users.noreply.github.com>
2026-04-25 14:36:28 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "Stopping old container..."
 docker stop myapp || true
 docker rm myapp || true
+
+echo "Freeing port 80..."
+sudo fuser -k 80/tcp || true
 
 echo "Logging into ECR..."
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 720538232148.dkr.ecr.us-east-1.amazonaws.com
@@ -12,3 +17,5 @@ docker pull 720538232148.dkr.ecr.us-east-1.amazonaws.com/python-code:latest
 
 echo "Running container..."
 docker run -d -p 80:5000 --name myapp 720538232148.dkr.ecr.us-east-1.amazonaws.com/python-code:latest
+
+echo "Deployment successful"
